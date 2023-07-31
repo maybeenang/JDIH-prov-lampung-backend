1615b8421e511c58894c76aba55cf290ae20a619
maybeenang <muhammad.120140194@student.itera.ac.id>
2023-08-01 01:25:21 +0700

    hehe

--- middleware/index.js
+++
@@ -1,7 +0,0 @@
-import authJwt from "./authJwt.js";
-import verifySignUp from "./verifySignUp.js";
-
-export default {
-  authJwt,
-  verifySignUp,
-};
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-import { authRoutes } from "./auth.routes.js";
-import { userRoutes } from "./user.routes.js";
-
-export const routes = (app) => {
-  authRoutes(app);
-  userRoutes(app);
-};
